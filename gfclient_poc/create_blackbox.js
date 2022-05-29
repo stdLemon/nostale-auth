1e9232337426c87489fcd06bb007b637d7c47deb
StdLemon <19240486+SqueezyLemon@users.noreply.github.com>
2022-05-29 11:48:02 +0200

    added tools

---
+++ gfclient_poc/create_blackbox.js
@@ -0,0 +1,9 @@
+#!/bin/node
+import fs from "fs"
+import {BlackboxEncoding} from "./blackbox_encoding.js"
+
+const filename = process.argv[2]
+const fingerprint = JSON.parse(fs.readFileSync(filename, {encoding: "utf8", flag: 'r'}));
+const blackbox = BlackboxEncoding.encode(fingerprint)
+process.stdout.write(blackbox)
+
